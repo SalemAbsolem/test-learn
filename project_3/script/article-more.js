c85a3e220b11b495f0d2f8cf81f6e9ee28d4af8f
SalemAbsolem <128633228+SalemAbsolem@users.noreply.github.com>
2023-05-15 13:58:29 +0300

    ошибка

--- project_3/script/article-more.js
+++
@@ -1,8 +0,0 @@
-let articles = document.querySelectorAll('.blog-article');
-
-for (let article of articles) {
-    let buttonMore = article.querySelector('.more');
-    buttonMore.onclick = function () {
-        article.classList.remove('short');
-    };
-}
